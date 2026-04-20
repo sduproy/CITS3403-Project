d51049d38070e4c6e636a620b10af01bd1ef5d12
vfbmdcccxciii <24467305@student.uwa.edu.au>
2026-04-20 17:16:11 +0800

    add: /community route in app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('itinerary.html')
 
+@app.route('/community')
+def community():
+    return render_template('community.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
